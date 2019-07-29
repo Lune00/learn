4f25afb9ffb60dbd9a000018aacd621fb71a1b6e
paul schuhmacher - macbook <polynomial_c@hotmail.fr>
2019-07-29 18:47:41 +0200

    update

--- C++/testgeneticdiversity.cpp
+++ C++/testgeneticdiversity.cpp
@@ -3,14 +3,56 @@
 #include<math.h>
 #include<vector>
 #include<cstdlib>
+#include<iomanip>
 
 using namespace std;
 
 
+double mean(const std::vector<double>& values ) {
+  if(values.size() == 0 ) return 0. ;
+  double mean = 0. ;
+  for(int i = 0 ; i != values.size() ; i++ ) {
+    mean += values[i] ;
+  }
+  return mean/(double)(values.size());
+}
 
+double var(const std::vector<double>& values ) {
+  if(values.size() == 0 ) return 0. ;
+  double meanSquared = 0. ;
+  for(int i = 0 ; i != values.size() ; i++ ) {
+    meanSquared += values[i] * values[i] ;
+  }
+  meanSquared /= (double)values.size() ;
+  double m = mean(values) ;
+  return (meanSquared - m * m);
+}
+
+double min(const std::vector<double>& values) {
+
+  if (values.size() == 0 ) return 0. ;
+  double minVal = values[0] ;
+
+  for(int i = 0 ; i != values.size() ; i++ ) {
+    minVal = min(values[i],minVal) ;
+  }
+  return minVal ;
+}
+
+double max(const std::vector<double>& values) {
+
+  if (values.size() == 0 ) return 0. ;
+  double maxVal = values[0] ;
+
+  for(int i = 0 ; i != values.size() ; i++ ) {
+    maxVal = max(values[i],maxVal) ;
+  }
+  return maxVal ;
+}
 
 void writePdf(const std::vector<double>& values) {
 
+  if(values.size() == 0 ) return ;
 
   const unsigned int nBins = 50 ; 
   double min = 0. ;
@@ -43,13 +85,82 @@ void writePdf(const std::vector<double>& values) {
 
 
 
+struct weightedNumber {
+  unsigned int number_ ;
+  unsigned int w_ ;
+  weightedNumber(int number, int w) : number_(number), w_(w) {} ;
+};
+
+int sumWeights( const std::vector<weightedNumber>& wn ) {
+  int sum = 0 ;
+  for(int i= 0; i!= wn.size() ; i++){
+    sum += wn[i].w_ ;
+  }
+  return sum ;
+}
+
+int weightedRandomChoice( const std::vector<weightedNumber>& alleles ) {
+
+  //1) Generate a random number between 0 and 1 :
+  double r = (random() / (double)RAND_MAX) ;
+  r *= sumWeights( alleles ) ;
+  for(int i = 0 ; i!= alleles.size() ; i++ ) {
+    r -= alleles[i].w_ ;
+    if ( r < 0. ) return alleles[i].number_ ;
+  }
+
+}
 
 
-int main(){
+//Generate genes expression with weighted random choice of alleles.
+//Really nice resulsts. Allow to control the distribution
+void generateSampleWithRandomCombinaisonWeightedAllelesDistribution( ) {
+
+  //// Weighted alleles test :
+  std::vector<weightedNumber> alleles ;
+
+  //Pool of initially present alleles with weigh. Modelise the initial population
+  alleles.push_back(weightedNumber( 0 , 1)) ;
+  alleles.push_back(weightedNumber( 1 , 1)) ;
+  alleles.push_back(weightedNumber( 2 , 1)) ;
+  alleles.push_back(weightedNumber( 3 , 4)) ;
+  alleles.push_back(weightedNumber( 4 , 5)) ;
+  alleles.push_back(weightedNumber( 5 , 60)) ;
+  alleles.push_back(weightedNumber( 6 , 7)) ;
+  alleles.push_back(weightedNumber( 7 , 8)) ;
+  alleles.push_back(weightedNumber( 8 , 1)) ;
+  alleles.push_back(weightedNumber( 9 , 1)) ;
+
+  int nSamples = 500000 ;
+  //We simulation the expression of a Feature with nGenes genes
+  int nGenes = 4 ;
+
+  std::vector<double> data ;
+
+  for (int sample = 0 ; sample < nSamples  ; sample++ ) {
+    double v = 0. ;
+    for( int n = 0 ; n < nGenes ; n++) {
+      unsigned int a = weightedRandomChoice( alleles ) ;
+      unsigned int b = weightedRandomChoice( alleles ) ;
+      double coeff = (random() / (double)RAND_MAX) ;
+      v += coeff * a + ( 1 - coeff) * b ;
+    }
+    //Expression of the feature
+    v /= (double)nGenes ;
+    data.push_back( v ) ;
+  }
+
+  writePdf(data) ;
+  cout << std::setprecision(2) <<  "mean : " << mean(data) << " var : " << var(data) << " min : " << min(data) <<" max : " << max(data) << endl ;
+
+  return ;
+
+}
 
 
-  ofstream myfile ;
-  myfile.open("data.txt");
+// Generate sample with random combinaisons of alleles : choose n( =ngenes) random pairs among N(nAlleles)
+// with n genes expressions for each final value of the feature v
+void generateSampleWithRandomCombinaisonsUniformAllelesDistribution() {
 
   int nalleles = 10 ;
   int nGenes = 3 ;
@@ -58,37 +169,28 @@ int main(){
   std::vector<double> data ;
 
   for (int sample = 0 ; sample < nSamples  ; sample++ ) {
-
-    // Generate sample data : Global exploration
-    //for( int i = 0 ; i < 10 ; i++) {
-    //  for( int j = i  ; j < 10 ; j++) {
-    //    double coeff = (random() / (double)RAND_MAX) ;
-    //    //double coeff = 0.5 ;
-    //    double v = coeff * i + ( 1 - coeff) * j ;
-    //    data.push_back( v ) ;
-    //  }
-    //}
-
-    // Generate sample with random combinaisons of alleles : choose n( =ngenes) random pairs among N(nAlleles)
-    // with n genes expressions for each final value of the feature v
     double v = 0. ;
     for( int n = 0 ; n < nGenes ; n++) {
       unsigned int a = rand() % nalleles ;
       unsigned int b = rand() % nalleles ;
-     // double coeff = (random() / (double)RAND_MAX) ;
-      double coeff = 0.3 ;
+      double coeff = (random() / (double)RAND_MAX) ;
+      //double coeff = 0.3 ;
       v += coeff * a + ( 1 - coeff) * b ;
     }
-
     v /= (double)nGenes ;
     data.push_back( v ) ;
   }
 
   //Statistics : mean , std , pdf , min , max
   writePdf(data) ;
+  cout << std::setprecision(2) <<  "mean : " << mean(data) << " var : " << var(data) << " min : " << min(data) <<" max : " << max(data) << endl ;
+  return ;
+}
 
-  myfile.close();
 
+int main(){
+
+  generateSampleWithRandomCombinaisonWeightedAllelesDistribution() ;
   return 0 ;
 
 }
