e59a5790d5bea7a5e9c64b06b240fe0c028da1fd
Lune00 <paul.schuhm@gmail.com>
2020-06-06 19:07:55 +0200

    use slots and props of modals

--- Vuejs/vuestic-admin/src/views/MyPage.vue
+++ Vuejs/vuestic-admin/src/views/MyPage.vue
@@ -2,24 +2,32 @@
 <!-- documentation officielle Vue sur slots: https://fr.vuejs.org/v2/guide/components-slots.html -->
 
 <template>
-<div id="">
-  <h2>Ma page pour tester 2 l'UI</h2>
-  <va-button small @click="show"> Un small bouton</va-button>
+  <div id="">
+    <h2>Ma page pour tester 2 l'UI</h2>
+    <va-button small @click="show"> Un small bouton</va-button>
 
-  <va-modal v-model="showModal" size="large">
-    <!-- On utilise le v-slot:default de la fenetre modale pour injecter notre contenu html personalisé -->
-    <template v-slot:default>
-      <p>Un paragraphe</p>
-      <p>Un autre paragraphe</p>
-      <ul>
-        <li>element 1</li>
-        <li>element 2</li>
-        <li>element 3</li>
-      </ul>
-    </template>
-  </va-modal>
-
-</div>
+    <va-modal
+    v-model="showModal"
+    size="large"
+    cancelText="Fermer"
+    title="Fenetre modale"
+    message="Hello tout le monde"
+    >
+      <!-- On utilise le v-slot:default de la fenetre modale pour injecter notre contenu html personalisé -->
+      <template v-slot:header>
+        <p>Fenêtre modale</p>
+      </template>
+      <template v-slot:default>
+        <p>Un paragraphe</p>
+        <p>Un autre paragraphe</p>
+        <ul>
+          <li>element 1</li>
+          <li>element 2</li>
+          <li>element 3</li>
+        </ul>
+      </template>
+    </va-modal>
+  </div>
 </template>
 <script>
 export default {
