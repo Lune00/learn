10262a8b1b5effa443cb46e9c80de36d38d0ac16
Lune00 <paul.schuhm@gmail.com>
2020-06-06 18:59:57 +0200

    ma page et exp modals with slots

--- Vuejs/vuestic-admin/src/views/MyPage.vue
+++ Vuejs/vuestic-admin/src/views/MyPage.vue
@@ -1,16 +1,39 @@
+<!-- Documentation sur modal : https://github.com/epicmaxco/vuestic-admin/wiki/Modals -->
+<!-- documentation officielle Vue sur slots: https://fr.vuejs.org/v2/guide/components-slots.html -->
+
 <template>
 <div id="">
   <h2>Ma page pour tester 2 l'UI</h2>
-  <va-button small> Hello 444   qsdqsd Button</va-button>
+  <va-button small @click="show"> Un small bouton</va-button>
+
+  <va-modal v-model="showModal" size="large">
+    <!-- On utilise le v-slot:default de la fenetre modale pour injecter notre contenu html personalisé -->
+    <template v-slot:default>
+      <p>Un paragraphe</p>
+      <p>Un autre paragraphe</p>
+      <ul>
+        <li>element 1</li>
+        <li>element 2</li>
+        <li>element 3</li>
+      </ul>
+    </template>
+  </va-modal>
+
 </div>
 </template>
 <script>
 export default {
   name: "MyPage",
   data: () => ({
-
-  })
-}
+    showModal: false
+  }),
+  methods: {
+    show() {
+      console.log(this.showModal);
+      this.showModal = true;
+      console.log("click");
+    }
+  }
+};
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
